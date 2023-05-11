b5e17a31f0d248955c49d3b88c7ab06ee297539b
gja5899 <anh-duc.thai@teliacompany.com>
2023-05-11 11:51:56 +0300

    prepare to deploy #4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,48 @@
-import ClientOnly from "./components/ClientOnly";
-import RegisterModal from "./components/modals/RegisterModal";
-import NavBar from "./components/navbar/NavBar";
-import "./globals.css";
+import { Nunito } from 'next/font/google'
 
-import { Nunito } from "next/font/google";
-import ToasterProvider from "./providers/ToasterProvider";
-import LoginModal from "./components/modals/LoginModal";
-import getCurrentUser from "./actions/getCurrentUser";
-import { SafeUser } from "./types";
-import RentModal from "./components/modals/RentModal";
-import SearchModal from "./components/modals/SearchModal";
-// reserver constant where u can conrol ur title and otehr option
+import NavBar from './components/navbar/NavBar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from '@/app/providers/ToasterProvider';
+
+import './globals.css'
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
+
 export const metadata = {
-  title: "Air CNC",
-  description: "Air BnB clone",
-};
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
 
-// expose class name which we can give to body elem
-const font = Nunito({
-  subsets: ["latin"],
+const font = Nunito({ 
+  subsets: ['latin'], 
 });
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <SearchModal />
           <LoginModal />
           <RegisterModal />
+          <SearchModal />
           <RentModal />
           <NavBar currentUser={currentUser} />
         </ClientOnly>
-
-        <div className="pb-20 pt-28">{children}</div>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
       </body>
     </html>
-  );
+  )
 }
